0e39715259a3fd009fe6608d6ed09e069c4544b1
Fi Dunnington <fi.dunnington@gmail.com>
2023-01-30 16:17:09 +0100

    html structure of planner plus current date in top banner aded

---
+++ assets/scripts/script.js
@@ -0,0 +1,8 @@
+//all js to go inside doc ready function to prevent errors on incompletely loaded pages
+$(document).ready(function(){
+
+    // create variables for interactive HTML elements
+    const today = $("#currentDay").text(moment().format("ddd DDD MMM YYYY"));
+    var save = $(".save");
+
+});
