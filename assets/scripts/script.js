6d3f904f180795313c3a7a6cb9a2934be0781a05
Fi Dunnington <fi.dunnington@gmail.com>
2023-01-30 16:21:28 +0100

    added currentTime function tocompare current time (moment.js) with planner times, and colour-code the planner rorows according to past/present/future. Also started writing event listeners for save and delete buttons, but not yet complete

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,4 +5,56 @@ $(document).ready(function(){
     const today = $("#currentDay").text(moment().format("ddd DDD MMM YYYY"));
     var save = $(".save");
 
+
+    function currentTime() {
+       
+        let currentTime = moment().hour();
+        console.log(currentTime);
+
+        $(".timeblock").each(function(){
+            let chosenTime = parseInt($(this).attr("id").split("-")[1]);
+            console.log(chosenTime);
+
+            if (currentTime < (chosenTime-1)) {
+                $(this).addClass('future');
+            } else if (currentTime > (chosenTime-1)) {
+                $(this).addClass('past');
+            } else {
+                $(this).addClass('present');
+            };
+        });
+    };
+    currentTime();
+
+ 
+    //eventListeners
+    $(".saveBtn").on("click", function(){
+
+        let timeblockId = $(this).parent().parent().attr("id");
+        let plannerText = $(this).parent().siblings("#plannerText").val();
+        let time = $(this).parent().siblings(".time").text();
+
+        
+        //set to local storage
+
+        //localStorage.setItem(".timeblock", value);
+
+        console.log('Save button works');
+        console.log(timeblockId);
+        console.log(plannerText);
+        console.log(time);
+
+        $(".notifications").addClass("show");
+
+        //notification timeout function
+        setTimeout(function(){
+            $(".notifications").removeClass("show");
+        },2000);
+
+    });
+
+    $(".trashBtn").on("click", function(){
+        console.log('Trash button works');
+    });
+
 });
